9ab0b363e46ee7308b758ac30e58e94ff91a5234
Zoe  Edge <zoemedge@Zoes-MacBook-Pro.local>
2023-05-08 16:06:11 -0500

    making the timer

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,10 +1,15 @@
+// where all my variables are.
 var startBtn = document.querySelector('#start-btn');
-var answerBtn = document.querySelector('.answer')
-var submitBtn = document.querySelector('.submit')
-var startEl = document.querySelector('#start')
-var quizEl = document.querySelector('#quiz')
-var endEl = document.querySelector('#end')
+var answerBtn = document.querySelector('.answer');
+var submitBtn = document.querySelector('.submit');
+var startEl = document.querySelector('#start');
+var quizEl = document.querySelector('#quiz');
+var endEl = document.querySelector('#end');
+// Should i put let or var?
+let remainingTimeEL = document.querySelector('#remainingTime');
+var secondsLeft = 100;
 
+// the eventLiteners that allow the switch between start, quiz, and end.
 startBtn.addEventListener ('click', function () {
     startEl.style.display = 'none';
     quizEl.style.display = null;
@@ -22,3 +27,16 @@ submitBtn.addEventListener ('click', function () {
     quizEl.style.display = 'none';
     endEl.style.display = 'none';
 });
+
+// making the timer 
+
+const downloadTimer = setInterval( 
+    () => {
+        if (secondsLeft <= 0) {
+        clearInterval(downloadTimer);
+        };
+        remainingTimeEL.value = secondsLeft;
+        remainingTimeEL.textContent = secondsLeft;
+        secondsLeft -= 1;
+},
+1000)
